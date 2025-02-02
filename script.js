14b5ce4342b2e931f6d20e3fb5803592c2942723
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2025-02-03 00:53:42 +0600

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ var dataTitle;
 var dataEpisode;
 var dataURL;
 
-const apiEndpoint = "api.consumet.org";
+const apiEndpoint = "consumet-api-goep.onrender.com";
 
 // Randomly set player iframes to these video onLoad
 var youtubeLinks = [
